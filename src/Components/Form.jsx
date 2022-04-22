1f7abf0a9b37b1bd6d6ef6489f42e47efc912995
Rodzertinas <mr.rodzertinas@inbox.lt>
2022-04-22 23:33:26 +0300

    movie
    Signed-off-by: Rodzertinas <mr.rodzertinas@inbox.lt>

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -10,8 +10,8 @@ function Form() {
 
   useEffect(() => {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1b359e5e7144c9fea70db6d5bb29245d&language=en-US&query=${inputText}`)
-      .then(res => setApi(res.data.results));
-    // console.log(data)
+      .then(res =>{console.log(res); return setApi(res.data.results)});
+   
 
 
   }, [inputText]);
@@ -24,7 +24,6 @@ function Form() {
   return (
     <>
       <input style={{
-
         width: '1000px',
         height: '50px',
         background: 'red'
