265200d9705a108c2d0645fb682f80353a10aec3
Rodzertinas <mr.rodzertinas@inbox.lt>
2022-04-23 22:16:45 +0300

    up
    Signed-off-by: Rodzertinas <mr.rodzertinas@inbox.lt>

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,24 +1,42 @@
 
 // import { v4 as uuidv4 } from 'uuid';
 // key = {uuidv4()}
+import star from '../Components/star.svg';
+ { <img className='star' src={star} alt=''/> }
+
 
 function MovieList({ filmai }) {
-   
+
+
+
+    const  balas = `/10 `;
+
+console.log(balas.toLowerCase());
+<p className='balas'>{balas}</p>
+
+
     return (
+     
 
         <div className='movie_list'>
-                    {
-                        filmai.map(filmas => 
-                        <div className="all_list">
-                            <h2 className='movie_title'>{filmas.title}</h2>
-                            <p className='movie_language'>{`Original language: `}{filmas.original_language}</p>
-                            <p className="movie_popularity">{filmas.popularity} </p>
-                            <p className='movie_overview'>{filmas.overview}</p>
-                            <img className='pic' src={`https://image.tmdb.org/t/p/original/${filmas.poster_path}`} alt="pic" />
-                        </div>
+            {
+                filmai.map(filmas =>
+                    <div className="all_list">
+                        <h2 className='movie_title'> {filmas.title}</h2>
+                        <p className='movie_language'>  {`Original language: `}{filmas.original_language.toUpperCase()}</p>
+                        <p className="movie_popularity">
+                        <img className='popularity_star' src={star} alt=''/>{filmas.vote_average}{`/10`}</p>
+                        <p className='movie_vote'>{filmas.vote_count}{` votes `}</p>
+                        <p className='movie_overview'>{filmas.overview}</p>
+                        <img className='pic' src={`https://image.tmdb.org/t/p/original/${filmas.poster_path}`} alt="pic" />
+                    </div>
+                    
+                            
+                             
                         
-                        )
-                    }
+
+                )
+            }
         </div>
 
     )
